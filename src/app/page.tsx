5d5abfee419c6992fda51cdbdb26c87dfd9864a0
Alejandro Heredia <alex.heredia.barbosa@gmail.com>
2023-12-26 22:01:54 -0500

    improved layout and optimized for mobile

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,32 +1,34 @@
 export default function Home() {
   return (
-    <main className='min-h-screen flex flex-col items-center p-4 tracking-wide font-roboto h-screen'>
-      <div className='w-1/2 flex flex-col space-between h-screen'>
+    <main className='min-h-screen flex flex-col items-center p-6 tracking-wide font-roboto h-screen m-auto'>
+      <div className='max-w-2xl flex flex-col space-between h-screen '>
         <section>
           <div className="mt-10">
             <nav className='flex flex-row items-center justify-between'>
-            <a className="inline-block"> <span ><h1 className="font-bold text-xl">Alejandro Heredia</h1></span></a>
-              <div className='flex space-x-4 w-1/2 justify-between font-thin align-center'>
-                <NavbarItem label='Linkedin' href=''/>
-                <NavbarItem label='Github' href=''/>
-                <NavbarItem label='Twitter' href=''/>
+            <a className="inline-block"> <span ><h1 className="font-bold md:text-xl text-lg">Alejandro Heredia</h1></span></a>
+              <div className='flex sm:w-2/5 justify-between font-thin align-center sm:text-base gap-3'>
+                <NavbarItem label='Linkedin' href='https://linkedin.com/in/alejoheredia/'/>
+                <NavbarItem label='Github' href='https://github.com/alejoheredia'/>
                 <NavbarItem label='Blog' href=''/> 
               </div>
             </nav>
           </div>
         </section>
-        <section className="w-4/5 mt-20 flex-grow">
-          <h1 className="font-bold text-3xl">Hello World, I'm Alejandro Heredia</h1>
+        <section className="w-7/8 md:w-4/5 mt-28 flex-grow">
+          <h1 className="font-bold md:text-3xl text-2xl">Hello World, I'm Alejandro Heredia</h1>
           <div className="text-justify w-10/12 mt-5 font-light">
             <span>
-              Passionate about engineering, computers, open source software, data and economics. I've been coding for more than 10 years and have had experience working from junior software developer to tech manager. Currently finalizing my economics major @ Universidad EAFIT. <br/>
+              Passionate about engineering, computers, open source software, data and economics. I've been coding for more than 10 years and have had experience working in roles from junior software developer to tech manager. Currently finalizing my economics major @ Universidad EAFIT. <br/>
             </span>
             <span className="inline-block mt-3">Based in Medellín, Colombia 🇨🇴.</span>
           </div>
-          <button className="w-36 h-12 bg-[#8C8C8C] font-bold rounded mt-5">Contact Me</button>
+        
+          <a className="inline-block px-8 py-3 bg-[#8C8C8C] font-bold rounded mt-5 text-center hover:bg-[#706F6F]" href="https://twitter.com/messages/compose?recipient_id=531661272" target="_blank">Contact Me</a>
+         
         </section>
         <footer className="h-10">
-          <a className="font-thin border-b border-gray-300 transition-[border-color] hover:border-[#8C8C8C]" target="_blank">Source</a>
+          <a className="font-thin border-b border-gray-300 transition-[border-color] hover:border-[#8C8C8C] md:text-base text-sm" target="_blank" href="https://github.com/alejoheredia/blog">Source</a>
+          <a className="font-thin border-b border-gray-300 transition-[border-color] hover:border-[#8C8C8C] md:text-base text-sm float-right" target="_blank" href="https://twitter.com/alejoherediab">@alejoheredia</a>
         </footer>
       </div>
     </main>
